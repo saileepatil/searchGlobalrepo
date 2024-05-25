af083088b39dd2dea6df8fa6b1408626a6aa4990
sailee <saileeap1409@gmail.com>
2024-05-25 17:20:49 +0530

    update is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
+import axios from 'axios';
+import "./App.css";
 
-function App() {
+function App(){
+
+ let  [location,setLocation] =useState("")
+ let [locationName,setLocationName]=useState([])
+
+   useEffect(()=>{
+axios.get(`https://restcountries.com/v3.1/name/${location}`)
+.then((res)=>{
+setLocationName(res.data);
+}).catch((err)=>{
+console.log(err);
+})
+   },[location])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='searchStyle'>
+<div className='searchBox'>
+<input
+value={location}
+onChange={(e)=>{
+setLocation(e.target.value)
+}}
+class="form-control " type="search" placeholder="Type Something?" />
+
+<div className='search-btn' >
+<i class="fa-solid fa-magnifying-glass"></i>
+</div>
+</div>
+
+<ol>
+{locationName.map((suggestion,i)=>{
+        return <li className='listName'>{suggestion.name.common}</li>
+  
+    })}
+</ol>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
